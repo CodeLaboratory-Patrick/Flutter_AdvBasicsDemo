78491ced5d9f69a72f1ca6711390310c3bb4d73f
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-18 11:16:10 +0100

    Configuring a column

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -7,12 +7,33 @@ class QuestionsScreen extends StatefulWidget {
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
-} 
+}
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Hello World');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(''),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 } 
 
