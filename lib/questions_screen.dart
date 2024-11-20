cd2c5b476c86c3193b80823057e208d05661d9cb
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-20 17:15:06 +0100

    Using Third-Party Packages & Adding Google Fonts.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 
@@ -12,14 +14,14 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-var currentQuestionIndex = 0;
+  var currentQuestionIndex = 0;
 
-void answerQuestion() {
-  setState(() {
-    //currentQuestionIndex += 1;
-    currentQuestionIndex++;
-  });
-} 
+  void answerQuestion() {
+    setState(() {
+      //currentQuestionIndex += 1;
+      currentQuestionIndex++;
+    });
+  }
 
   @override
   Widget build(context) {
@@ -28,18 +30,18 @@ void answerQuestion() {
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: const EdgeInsets.all(40), 
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center
-            ),
+            Text(currentQuestion.text,
+                style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
